161bec4daff47dd2a7be2277a6f09b52b465bccf
harry.zhang. <harry.zhang@sap.com.>
2026-03-21 12:27:52 +0800

    Initial Code sources of platform

---
+++ src/main/java/com/company/IntelligentPlatform/IntelligentPlatformApplication.java
@@ -0,0 +1,12 @@
+package com.company.IntelligentPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelligentPlatformApplication.class, args);
+    }
+}
